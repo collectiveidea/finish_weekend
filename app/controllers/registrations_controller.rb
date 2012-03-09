a7114ddd31830201b367f173fe575bfe3db06122
Steve Richert <steve.richert@gmail.com>
2012-03-09 10:57:09 -0600

    Build the basic registration form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,20 @@
 class RegistrationsController < ApplicationController
   respond_to :html
 
+  before_filter :load_event
+
   def new
+    @registration = @event.registrations.build
+    respond_with @registration
   end
 
   def create
+    @registration = @event.registrations.create(params[:registration])
+    respond_with @registration, location: @event
   end
+
+  private
+    def load_event
+      @event = Event.find_by_slug!(params[:event_id])
+    end
 end
