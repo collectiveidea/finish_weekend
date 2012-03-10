9eec5b56401d8ebcc0d748cff2056d5ee5bc4933
Steve Richert <steve.richert@gmail.com>
2012-03-10 16:17:48 -0600

    Fix Cucumber payment helpers to account for paginated Stripe API requests for charges

--- features/step_definitions/payment_steps.rb
+++ features/step_definitions/payment_steps.rb
@@ -1,3 +1,3 @@
 Then /^\$(\d+(?:\.\d{2})?) should have been charged$/ do |amount|
-  new_payments.should == amount.to_f
+  charged.should == amount.to_f
 end
--- features/support/payment.rb
+++ features/support/payment.rb
@@ -1,19 +1,27 @@
 module PaymentHelpers
-  def payments
-    Stripe::Charge.all.data.sum(&:amount).to_f / 100
+  def charged
+    new_charges.sum(&:amount).to_f / 100
   end
 
-  def old_payments
-    @old_payments ||= payments
+  def new_charges
+    charges.select{|c| c.created > latest_charge.try(:created).to_i }
   end
 
-  def new_payments
-    payments - old_payments
+  def charges
+    Stripe::Charge.all.data.sort_by(&:created).reverse
+  end
+
+  def latest_charge
+    old_charges.first
+  end
+
+  def old_charges
+    @old_charges ||= charges
   end
 end
 
 World(PaymentHelpers)
 
 Before "@payment" do
-  old_payments
+  old_charges
 end
