df7e7d2d94ffa6968c86f019311e548a0512b329
Tim Bugai <tim@bugai.net>
2012-04-18 12:10:45 -0400

    added an event to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Event.create :name => "Boston", :start_date => "2012-03-24", :end_date => "2012-03-25"
