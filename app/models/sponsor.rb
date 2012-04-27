82b8b8af76ba75900c64b4bd406969494f4fe59b
Tim Bugai <tim@bugai.net>
2012-04-27 12:24:51 -0400

    maked it pretty

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,10 +1,14 @@
 class Sponsor < ActiveRecord::Base
-  SPONSOR_LEVELS = %w(Partner Event)
+  SPONSOR_LEVELS = %w(Partner Premier Event)
 
   belongs_to :event
 
   mount_uploader :image, ImageUploader
 
-  scope :partner, where(:level => 'Partner')
+  scope :premier, where(:level => 'Premier')
   scope :event, where(:level => 'Event')
+
+  def self.partner
+    where(:level => 'Partner').first
+  end
 end
