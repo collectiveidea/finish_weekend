f765eae00adbc4c6ca014298cd382d298fb2ec26
Tim Bugai <tim@bugai.net>
2012-05-02 09:12:28 -0400

    added a sponsor information page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 FinishWeekend::Application.routes.draw do
   get "/faq", :to => "home#faq"
+  get "/sponsors", :to => "home#sponsors"
 
   resources :events, only: [:index, :show] do
     resources :registrations, only: [:new, :create] do
